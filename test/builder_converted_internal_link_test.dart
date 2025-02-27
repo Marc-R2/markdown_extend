456ec0cbe252d04366931d8b6ba55a0df9eb000e
Marc Renken <marc.renken@outlook.de>
2025-02-27 08:36:49 +0100

    test: Converted - InternalLink

---
+++ test/builder_converted_internal_link_test.dart
@@ -0,0 +1,59 @@
+import 'package:markdown_extend/src/builder.dart';
+import 'package:markdown_extend/src/converted/internal_link.dart';
+import 'package:markdown_extend/src/converted/text.dart';
+import 'package:markdown_extend/src/token/atomic.dart';
+import 'package:markdown_extend/src/token/group.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late Builder builder;
+
+  const tokenAtomic0 = TokenAtomic('Test');
+  const tokenAtomic0txt = "TokenAtomic('Test')";
+  const tokenAtomic1 = TokenAtomic('test');
+  const tokenAtomic1txt = "TokenAtomic('test')";
+
+  const tokenGroup0 = TokenGroup([tokenAtomic0, tokenAtomic1]);
+  const convertedText0 = ConvertedText(tokenAtomic0);
+  const convertedText1 = ConvertedText(tokenGroup0);
+
+  const convertedFile0 = InternalLinkConverted(tokenAtomic0, convertedText0);
+  const convertedFile1 = InternalLinkConverted(tokenAtomic1, convertedText1);
+
+  setUp(() {
+    builder = Builder();
+  });
+
+  test('create InternalLinkConverted with TokenAtomic', () {
+    convertedFile0.build(builder);
+
+    expect(builder.variableNames, hasLength(equals(3)));
+    expect(builder.listBuilder, hasLength(equals(3)));
+
+    final build = builder.build();
+    expect(build, contains('const  var0 = $tokenAtomic0txt;'));
+    expect(build, contains('const  var1 = ConvertedText(var0);'));
+    expect(build, contains('const  var2 = InternalLinkConverted(var0, var1);'));
+  });
+
+  test('create InternalLinkConverted with TokenGroup', () {
+    convertedFile1.build(builder);
+
+    expect(builder.variableNames, hasLength(equals(5)));
+    expect(builder.listBuilder, hasLength(equals(5)));
+
+    final build = builder.build();
+    expect(build, contains('const  var1 = $tokenAtomic0txt;'));
+    expect(build, contains('const  var2 = $tokenAtomic1txt;'));
+    expect(build, contains('const  var0 = TokenGroup([var1, var2]);'));
+    expect(build, contains('const  var3 = ConvertedText(var0);'));
+    expect(build, contains('const  var4 = InternalLinkConverted(var2, var3);'));
+  });
+
+  test('create same InternalLinkConverted twice', () {
+    final var0 = convertedFile1.build(builder);
+    final var1 = convertedFile1.build(builder);
+
+    expect(var0, equals(var1));
+  });
+}
